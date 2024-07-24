1276638d0a2b8e65f326e03e560b2de88707b859
Jojo Mensah <jojomensah89@gmail.com>
2024-07-24 08:44:19 +0000

    feat: Update welcome message on landing page

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 py-20">
-      Welcome to the Home Page
+      Welcome to the Home Page new
     </div>
   );
 };
